b1ca6b4c611d6d0ca36c9e93f42393dbb4a6dc48
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-12-18 00:47:59 -0300

    finished lesson 15

---
+++ MODULO 4 - DATA DEFINITION (MANAGING DE TABLES E TRABALHO COM DATA TYPES)/queries/inserting_data_into_users_table.sql
@@ -0,0 +1,35 @@
+-- INSERT INTO users VALUES    --os values inseridos deverão ser inseridos/passados por ORDEM...
+
+
+
+
+
+
+
+
+
+--VARIANTE DE 'insert into' MENOS USADA (considera a ORDEM de colunas na sua table)
+INSERT INTO users VALUES('Arthur Panazolo', 24000, 'employed');  --sempre use '' WRAPPANDO SUAS STRINGS A SEREM INSERIDAS/UPDATADAS, e nunca "" (pq isso resulta em ERROS)...
+
+
+
+
+
+
+--variante de 'INSERT INTO' MAIS USADA (DECLARA A ORDEM DOS FIELDS ANTERIORMENTE, PARA SÓ ENTÃO INSERIR VALUES NOS MESMOS...)
+
+
+-- essa variante TAMBÉM TE PERMITE ESPECIFICAR EM QUAIS FIELDS VOCÊ QUER INSERIR, E QUAIS FIELDS QUER DEIXAR INTACTO/NÃO QUER INSERIR COISA ALGUMA...
+
+
+
+-- exemplo em que inserimos data em TODOS OS FIELDS de uma row (todas as columns)
+INSERT INTO users (yearly_salary, full_name, current_status) VALUES (24000, 'Arthur Panazolo', 'employed');
+
+
+
+
+-- exemplo em que inserimos data em APENAS ALGUNS FIELDS de uma row (apenas o 'salary' e o 'full_name'; 'current-status' FICARÁ EM BRANCO)...
+INSERT INTO users (yearly_salary, full_name) VALUES (24000, 'Arthur Panazolo');
+
+
