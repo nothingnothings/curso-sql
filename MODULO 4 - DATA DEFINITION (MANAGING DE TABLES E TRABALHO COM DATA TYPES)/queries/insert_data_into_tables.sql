bf7fe53514499e033c7c297e2e6e34ee71653193
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-12-18 15:49:03 -0300

    finished lesson 22

---
+++ MODULO 4 - DATA DEFINITION (MANAGING DE TABLES E TRABALHO COM DATA TYPES)/queries/insert_data_into_tables.sql
@@ -0,0 +1,34 @@
+-- INSERT INTO employers (
+--         company_name,
+--         "address",
+--         yearly_revenue,
+--         ishiring
+--     ) VALUES ('fidelizou.me', 'OTTO', 200000, true);
+-- INSERT INTO employers (
+--         company_name,
+--         ishiring,
+--         yearly_revenue,
+--         "address"
+--     )
+-- VALUES ('learning.inc', false, 1000000, 'London');
+-- INSERT INTO employers (
+--         company_name,
+--         ishiring,
+--         yearly_revenue,
+--         "address"
+--     )
+-- VALUES ('Big Oil inc.', true, 5000000, 'France');
+-- INSERT INTO employers (
+--         company_name,
+--         ishiring,
+--         yearly_revenue,
+--         "address"
+--     )
+-- VALUES ('Hipster Food', true, 2500000, 'Belgium');
+INSERT INTO conversations (user_name, employer_name, message, date_sent)
+VALUES (
+        'Someone',
+        'Somewho',
+        'gibberish gibberish gibberish',
+         '2012-12-12'
+    );
