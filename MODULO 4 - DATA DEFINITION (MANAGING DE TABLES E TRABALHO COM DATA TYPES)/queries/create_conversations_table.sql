f198294aa012f7fd2733d66fe1474bd3c59bc86b
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-12-18 03:09:37 -0300

    finished lesson 20

--- MODULO 4 - DATA DEFINITION (MANAGING DE TABLES E TRABALHO COM DATA TYPES)/queries/create_conversations_table.sql
+++ MODULO 4 - DATA DEFINITION (MANAGING DE TABLES E TRABALHO COM DATA TYPES)/queries/create_conversations_table.sql
@@ -1,6 +1,6 @@
-CREATE TABLE (
-    message TEXT,
-    date_sent DATE(),
-    user TEXT,
-    employer TEXT
+CREATE TABLE conversations (
+    message TEXT, --STRINGS DE QUALQUER LENGTH
+    date_sent TIMESTAMP, --suportado PELO MYSQL E POSTGRESQL..
+    user_name VARCHAR(200), --strings de length 200 (200 bytes)
+    employer_name VARCHAR(200)
 );
