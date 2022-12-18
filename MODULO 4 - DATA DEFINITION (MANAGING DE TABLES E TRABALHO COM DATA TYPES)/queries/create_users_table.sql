6146cbbd21e10e3fddca9ac1bf69889afae569dc
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-12-17 23:18:53 -0300

    finished lesson 14

--- MODULO 4 - DATA DEFINITION (MANAGING DE TABLES E TRABALHO COM DATA TYPES)/queries/create_users_table.sql
+++ MODULO 4 - DATA DEFINITION (MANAGING DE TABLES E TRABALHO COM DATA TYPES)/queries/create_users_table.sql
@@ -1,10 +1,5 @@
-
-
 CREATE TABLE users (
-    full_name VARCHAR(200),   -- NÚMERO MÁXIMO DE CARACTERES: 200 (VARCHAR(X))
-    yearly_salary 
+    full_name VARCHAR(200),
+    -- NÚMERO MÁXIMO DE CARACTERES: 200 (VARCHAR(X))
+    yearly_salary INT,
 );
-
-
-
-
