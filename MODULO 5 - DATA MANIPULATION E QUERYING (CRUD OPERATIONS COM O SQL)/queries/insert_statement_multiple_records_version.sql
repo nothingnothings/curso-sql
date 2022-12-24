4616acde014602d573af2b601faaa539d8d0f033
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-12-24 17:42:50 -0300

    finished lesson 4

---
+++ MODULO 5 - DATA MANIPULATION E QUERYING (CRUD OPERATIONS COM O SQL)/queries/insert_statement_multiple_records_version.sql
@@ -0,0 +1,20 @@
+
+--  INSERT INTO nome_da_table (column1, column2, column3) VALUES ("OI", 2, "/SAASASAS/dasdsasad")  --versão comum
+
+
+
+
+
+
+
+ 
+ INSERT INTO nome_da_table (column1, column2, column3)  ---versão em MASSA
+VALUES 
+    ("OI", 2, "/SAASASAS/dasds2121asad") 
+    ("OI2", 23, "/SAASASAS/dasd31sasad") 
+    ("OI3", 3212132, "/SAASASAS/da2sdsasad") 
+    ("OI4", 2451241, "/SAASAdassadzcCCSAS/dasdsa1231sad") 
+    ("OI5", 1221122, "/SAASASAS/dasdsa5sassad") 
+
+
+
