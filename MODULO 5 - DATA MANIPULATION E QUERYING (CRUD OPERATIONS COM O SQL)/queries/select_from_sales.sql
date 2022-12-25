7bbd11e2751e5902c9657e2d28f5843c1d87f2a4
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-12-25 16:08:33 -0300

    finished lesson 13

---
+++ MODULO 5 - DATA MANIPULATION E QUERYING (CRUD OPERATIONS COM O SQL)/queries/select_from_sales.sql
@@ -0,0 +1,9 @@
+SELECT *
+FROM sales;
+--sintaxe mais basica possível de return de data de suas tables...
+--- como selecionar APENAS ALGUNS CAMPOS DE ALGUMA TABLE:
+SELECT customer_name,
+    product_name,
+    volume,
+    date_created
+FROM sales;
