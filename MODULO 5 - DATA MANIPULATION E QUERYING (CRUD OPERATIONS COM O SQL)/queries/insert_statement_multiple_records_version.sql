e307e98b4e95af8dea86f5f55ba27838df44931b
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-12-25 01:44:37 -0300

    finshed lesson 9

--- MODULO 5 - DATA MANIPULATION E QUERYING (CRUD OPERATIONS COM O SQL)/queries/insert_statement_multiple_records_version.sql
+++ MODULO 5 - DATA MANIPULATION E QUERYING (CRUD OPERATIONS COM O SQL)/queries/insert_statement_multiple_records_version.sql
@@ -1,20 +1,23 @@
-
 --  INSERT INTO nome_da_table (column1, column2, column3) VALUES ("OI", 2, "/SAASASAS/dasdsasad")  --versão comum
+--  INSERT INTO nome_da_table (column1, column2, column3)  ---versão em MASSA
+-- VALUES 
+--     ("OI", 2, "/SAASASAS/dasds2121asad") 
+--     ("OI2", 23, "/SAASASAS/dasd31sasad") 
+--     ("OI3", 3212132, "/SAASASAS/da2sdsasad") 
+--     ("OI4", 2451241, "/SAASAdassadzcCCSAS/dasdsa1231sad") 
+--     ("OI5", 1221122, "/SAASASAS/dasdsa5sassad") 
 
 
+INSERT INTO sales (
+    date_fulfilled,
 
-
-
-
-
- 
- INSERT INTO nome_da_table (column1, column2, column3)  ---versão em MASSA
-VALUES 
-    ("OI", 2, "/SAASASAS/dasds2121asad") 
-    ("OI2", 23, "/SAASASAS/dasd31sasad") 
-    ("OI3", 3212132, "/SAASASAS/da2sdsasad") 
-    ("OI4", 2451241, "/SAASAdassadzcCCSAS/dasdsa1231sad") 
-    ("OI5", 1221122, "/SAASASAS/dasdsa5sassad") 
-
-
-
+        customer_name,
+        product_name,
+        volume,
+        is_recurring,
+        is_disputed
+    )
+VALUES (NULL, 'Paul', 'Chair', 2000, true, true),
+(NULL, 'Chris', 'Table', 4000, true),
+(NULL, 'Ana', 'Tools', 2000, true),
+(NULL, 'John', 'Ladder', 2000, true);
