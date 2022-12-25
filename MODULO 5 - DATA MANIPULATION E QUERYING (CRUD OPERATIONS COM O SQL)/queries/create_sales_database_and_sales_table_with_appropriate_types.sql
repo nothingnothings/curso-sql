45d58c1ce8f74c73e1064f6d7f5b4a72ba11623a
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-12-24 21:48:57 -0300

    finished lesson 8

--- MODULO 5 - DATA MANIPULATION E QUERYING (CRUD OPERATIONS COM O SQL)/queries/create_sales_database_and_sales_table_with_appropriate_types.sql
+++ MODULO 5 - DATA MANIPULATION E QUERYING (CRUD OPERATIONS COM O SQL)/queries/create_sales_database_and_sales_table_with_appropriate_types.sql
@@ -1,11 +1,16 @@
 -- CREATE DATABASE sales;
+
+
+-- DROP TABLE sales; 
+
+
 CREATE TABLE sales (
     id SERIAL PRIMARY KEY,
-    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    date_fulfilled TIMESTAMP,
+    date_created DATE DEFAULT CURRENT_DATE,
+    date_fulfilled DATE,
     customer_name VARCHAR(255) NOT NULL,
     product_name VARCHAR(255) NOT NULL,
-    volume DECIMAL,
+    volume NUMERIC(10, 3) NOT NULL CHECK(volume > 0),
     is_recurring BOOLEAN DEFAULT false,
     is_disputed BOOLEAN DEFAULT false
 );
