83fa4665d7ea088dea76623bc6b2e0f2801f9817
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-12-25 23:35:51 -0300

    finished lesson 24

---
+++ MODULO 5 - DATA MANIPULATION E QUERYING (CRUD OPERATIONS COM O SQL)/queries/get_distinct_values_in_table.sql
@@ -0,0 +1,14 @@
+-- SELECT customer_name DISTINCT
+-- FROM sales;
+-- SELECT product_name DISTINCT   --- ganhamos TODOS OS VALUES DISTINTOS DE 'product_name' nessa table...
+-- FROM sales;
+
+
+
+
+-- SELECT DISTINCT customer_name
+-- ORDER BY customer_name;
+-- (
+--     ISSO VAI ORDENAR TODOS OS RESULTS,
+--     SEGUINDO A ORDEM ALFABÉTICA...
+-- )
