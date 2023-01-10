5f779bb909e16d332fdd8a637d244313f4853e43
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-01-10 13:24:16 -0300

    finished lesson 3

---
+++ MODULO 9 - QUERY FUNCTIONS BUILT-IN E OPERATORS/queries/mathematical_functions_and_operators_example.sql
@@ -0,0 +1,35 @@
+-- SELECT SUM(price * billing_frequency)
+-- from memberships;
+
+
+
+
+
+-- SÃO TODAS MATH FUNCTIONS:
+
+--  SELECT CEIL(consumption)
+--  FROM memberships;
+
+
+
+
+--  SELECT FLOOR(consumption)
+--  FROM memberships;
+
+
+
+
+
+ SELECT ROUND(consumption, 1)  --  O ROUND ARREDONDA, O 'TRUNC' ___ CORTA__ as casas decimais
+ FROM memberships;
+
+
+
+
+--  SELECT TRUNCATE(consumption) -- O ROUND ARREDONDA, O 'TRUNC' ___ CORTA__ as casas decimais
+--  FROM memberships;
+
+
+
+
+
