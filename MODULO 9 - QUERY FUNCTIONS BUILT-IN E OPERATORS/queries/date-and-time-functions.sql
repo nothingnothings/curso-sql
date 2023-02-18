2c404d8e73bbd5ba988c1cf1a2e2a062a51d1b82
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-02-18 10:40:53 -0300

    finished lesson 6

---
+++ MODULO 9 - QUERY FUNCTIONS BUILT-IN E OPERATORS/queries/date-and-time-functions.sql
@@ -0,0 +1,25 @@
+
+--  COM A KEYWORD/FUNCTION DE 'EXTRACT()', podemos EXTRAIR INFO ESPECÍFICA DE TIMESTAMPS/DATES...
+-- isso vai nos dar o mês, por exemplo...
+SELECT EXTRACT(MONTH FROM last_checkin) FROM memberships;
+
+
+
+
+
+-- SELECT EXTRACT(DAY FROM last_checkin)
+-- FROM memberships;
+
+
+
+-- SELECT EXTRACT(YEAR FROM last_checkin)
+-- FROM memberships;
+
+
+
+
+
+-- SELECT EXTRACT(MINUTE FROM last_checkin)
+-- FROM memberships;
+
+
