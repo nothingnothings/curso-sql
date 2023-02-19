662e483a4add86cdf48fa7d3a6c9935d582f3132
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-02-19 15:16:29 -0300

    finished lesson 4

---
+++ MODULO 10 - DATABASE TRANSACTIONS/queries/outsourced_customer_insert_statement.sql
@@ -0,0 +1,6 @@
+INSERT INTO customers (first_name, last_name, email)
+VALUES (
+        'Maria',
+        'Jones',
+        'maria@test.com'
+    );
--- MODULO 10 - DATABASE TRANSACTIONS/queries/two_insert_value_statements_transaction.sql
+++ MODULO 10 - DATABASE TRANSACTIONS/queries/two_insert_value_statements_transaction.sql
@@ -1,3 +1,6 @@
+
+
+--  COMPORTAMENTO DEFAULT (sem transactions) --> A PRIMEIRA QUERY É EXECUTADA E ADICIONA 'MARIE' NA TABLE DE CUSTOMERS; JÁ A SEGUNDA QUERY, QUE ESTÁ IRREGULAR, NÃO É EXECUTADA (não faz nada)..
 INSERT INTO customers (first_name, last_name, email)
 VALUES (
         'Maria',
@@ -6,3 +9,46 @@ VALUES (
     );
 INSERT INTO orders (amount_billed, customer_id)
 VALUES (103.12);
+
+
+
+
+-- COMPORTAMENTO COM TRANSACTION --> TODAS AS QUERIES SÃO EXECUTADAS __ APENAS__ SE TODAS AS QUERIES 'DÃO CERTo' (não estão irregulares/com problemas/não caiu a conexão)...
+
+
+
+
+
+
+
+
+
+-- PARA COMEÇAR 1 TRANSACTION (que continua/continuará ROLANDo enquanto você mantiver sua CONEXÃO/SESSÃO COM A DATABASE ABERTA...),
+-- SÃO UTILIZADAS AS EXPRESSÕES:
+
+
+
+
+START TRANSACTION -- EXPRESSÃO DO MYSQL 
+BEGIN -- EXPRESSÃO DO POSTGRES
+
+
+
+
+
+
+-- CERTO... ASSIM QUE ESSE COMANDO FOR EXECUTADO,
+
+-- isso 
+
+
+
+
+
+-- __ VAI __ 
+
+
+-- TRANSFORMAR NOSSA 'CURRENT SESSION' (CONEXÃO COM A DATABASE) 
+
+
+-- EM 1 TRANSACTION... ---------> 
