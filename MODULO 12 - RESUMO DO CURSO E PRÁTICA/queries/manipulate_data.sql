64c2c643d5ea2ef0f0bdc1a0146faf42d3b8ee9c
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-02-21 11:31:55 -0300

    finished lesson 17

---
+++ MODULO 12 - RESUMO DO CURSO E PRÁTICA/queries/manipulate_data.sql
@@ -0,0 +1,32 @@
+INSERT INTO events (
+        name,
+        date_planned,
+        image,
+        description,
+        max_participants,
+        min_age
+    )
+VALUES (
+        'Party',
+        '2022-10-29 16:30:00',
+         'www.google.com',
+        'Some party in a random place',
+        10,
+        16
+    ),
+    (
+        'Party 2',
+        '2021-10-29 16:30:00',
+         'www.yahoo.com',
+        'Who Knows',
+        12,
+        20
+    ),
+    (
+        'Party 3 ',
+        '2023-10-29 22:30:00',
+         'www.bing.com',
+        'Some party in a random place',
+        10,
+        16
+    )
