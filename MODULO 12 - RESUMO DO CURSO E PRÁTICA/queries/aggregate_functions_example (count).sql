198522726cde7a9e1052b9e988166226bffc8255
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-02-21 23:38:09 -0300

    Finished lesson 32

---
+++ MODULO 12 - RESUMO DO CURSO E PRÁTICA/queries/aggregate_functions_example (count).sql
@@ -0,0 +1,15 @@
+SELECT COUNT(*)
+FROM locations;
+
+
+-- DEPOIS DO COUNT,
+-- TEMOS OUTRAS AGGREGATE FUNCTIONS,
+-- como 'MIN()' e 'MAX()',
+-- USADOS PARA CONSEGUIR OS MAIORES E MENORES
+-- VALUES EM 1 GIVEN COLUMN...
+
+
+--TAMBÉM 'SUM()', que soma todos os values da column (que n sejam null)...
+
+
+-- TAMBÉM 'AVG()'...
